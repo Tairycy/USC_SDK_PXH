060c9b776e286d944f0b002ed752e71d1b3fe76d
pxh <peixinhua@unisound.com>
2016-10-12 13:55:26 +0800

    更新主题：工程提交 更新内容：

---
+++ USC_SDK/USC_SDK/USC_SDK.h
@@ -0,0 +1,13 @@
+//
+//  USC_SDK.h
+//  USC_SDK
+//
+//  Created by pxh on 2016/10/12.
+//  Copyright © 2016年 pxh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface USC_SDK : NSObject
+
+@end
---
+++ USC_SDK/USC_SDK/USC_SDK.m
@@ -0,0 +1,13 @@
+//
+//  USC_SDK.m
+//  USC_SDK
+//
+//  Created by pxh on 2016/10/12.
+//  Copyright © 2016年 pxh. All rights reserved.
+//
+
+#import "USC_SDK.h"
+
+@implementation USC_SDK
+
+@end
