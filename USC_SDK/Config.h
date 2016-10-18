f7fce7702b43812d71035eeb23e7efdba810b99b
pxh <peixinhua@unisound.com>
2016-10-18 17:07:36 +0800

    更新主题：更新配置信息 更新内容：

---
+++ USC_SDK/Config.h
@@ -0,0 +1,22 @@
+//
+//  Config.h
+//  USC_SDK
+//
+//  Created by pxh on 2016/10/18.
+//  Copyright © 2016年 pxh. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+/* version 版本更新信息
+ * 初始化引擎时不再开始录音
+ * 保证stop后流程可以结束，避免造成SDK的崩溃
+ * 适配iOS 10确保SDK在iOS 10上正常运行
+ * 裴新华更新于2016.10.18.1704
+ */
+
+#define v3.0.1
+
+
+#endif /* Config_h */
